8436a559cb7f2b9ca33e151bf8ecd6be57058782
MikhailAfanasjev <66779383+MikhailAfanasjev@users.noreply.github.com>
2021-11-21 18:14:53 +0300

    Add files via upload

---
+++ WindowsFormsApp3/WindowsFormsApp3/First.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    class First
+    {
+        private double cabel_type;
+        private double count_g;
+        private double diametr;
+        public First(double cabel_type, double count_g, double diametr)
+        {
+            this.cabel_type = cabel_type;
+            this.count_g = count_g;
+            this.diametr = diametr;
+        }
+        public virtual double Q()
+        {
+            return diametr / count_g;
+        }
+            
+    }
+}
---
+++ WindowsFormsApp3/WindowsFormsApp3/Second.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    class Second:First
+    {
+        private bool P;
+    
+        public Second(bool P, double cabel_type, double count_g, double diametr): base(cabel_type, count_g, diametr)
+        {
+            this.P = P;
+
+        }
+        public double Qp()
+        {
+            double result = 0;
+            if (P)
+                result = 2 * base.Q();
+            else result = 0.7 * base.Q();
+            return result;
+        }
+    }
+}
